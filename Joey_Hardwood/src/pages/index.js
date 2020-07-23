4d4a487dc6f1cb575a60364f3affc0bc9db85b93
Reed James <reedtjames1234@gmail.com>
2020-07-23 16:05:07 -0500

    index.js file updated

--- Joey_Hardwood/src/pages/index.js
+++ Joey_Hardwood/src/pages/index.js
@@ -1,5 +1,14 @@
 import React from "react"
 
-export default function Home() {
-  return <div>Hello world!</div>
+const IndexPage = () => {
+  return (
+    <div>
+      <h1>
+        Howdy!
+      </h1>
+      <h2>Dont mind me, im just an h2</h2>
+    </div>
+  )
 }
+
+export default IndexPage
