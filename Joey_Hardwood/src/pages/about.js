6e3c3d58a9a641bba1b2bf2419e84e9f741235be
Reed James <reedtjames1234@gmail.com>
2020-07-23 16:14:41 -0500

    about and contact page added

---
+++ Joey_Hardwood/src/pages/about.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const AboutPage = () => {
+    return (
+        <div>
+            <h1>About me page</h1>
+            <p>
+                Here is some filler text
+            </p>
+        </div>
+    )
+}
+
+export default AboutPage
---
+++ Joey_Hardwood/src/pages/contact.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ContactPage = () => {
+    return (
+        <div>
+            <h1>Contact page</h1>
+            <p>
+                Here is some filler text for the contact
+            </p>
+        </div>
+    )
+}
+
+export default ContactPage
